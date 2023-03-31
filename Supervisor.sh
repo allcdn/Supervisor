fb89a9580da6f03b02b47f045416bdf37746bd57
路人 <70009473+allcdn@users.noreply.github.com>
2023-03-31 13:55:18 +0800

    Update Supervisor.sh

--- Supervisor.sh
+++ Supervisor.sh
@@ -1,14 +1,14 @@
-#! bin/bash
+#!/bin/bash
 apt install -y epel-release
 apt remove -y supervisor
 apt install -y supervisor
 
 # 备份配置文件，新增个性化配置（无配置则使用默认配置）
-if [ ! -f "/etc/supervisord.example.conf" ]; then
-  mv -fu /etc/supervisord.conf /etc/supervisord.example.conf
+if [ ! -f "/etc/supervisor/supervisord.example.conf" ]; then
+  mv -fu /etc/supervisor/supervisord.conf /etc/supervisor/supervisord.example.conf
 fi
 
-cat <<EOF >/etc/supervisord.conf
+cat <<EOF >/etc/supervisor/supervisord.conf
 [unix_http_server]
 file=/var/run/supervisor/supervisor.sock
 
